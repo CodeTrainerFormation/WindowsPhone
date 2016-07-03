5f368a93a5c7fe7936464fc318b620283250d0f4
Azunyth <romain.rukse@codetrainer.fr>
2016-07-03 19:21:35 +0200

    Add 4-Json correction

---
+++ Corrections/4-Json/ViewModels/DetailViewModel.cs
@@ -0,0 +1,33 @@
+﻿using _4_Json.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace _4_Json.ViewModels
+{
+    public class DetailViewModel
+    {
+        public Restaurant Restaurant { get; set; }
+
+        public DetailViewModel()
+        {
+            this.LoadJsonAsync();
+        }
+
+        public async void LoadJsonAsync()
+        {
+            StorageFile sampleFile
+                = await StorageFile.GetFileFromApplicationUriAsync(new Uri(@"ms-appx:///Data/one_restaurant.json"));
+
+            string contentFileJson = await FileIO.ReadTextAsync(sampleFile);
+
+            Restaurant restaurant = JsonConvert.DeserializeObject<Restaurant>(contentFileJson);
+
+            this.Restaurant = restaurant;
+        }
+    }
+}
