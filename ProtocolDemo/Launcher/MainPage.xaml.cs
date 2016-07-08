f7be171c8f624cf0c55809957aeec778db6045dd
Azunyth <romain.rukse@codetrainer.fr>
2016-07-08 10:40:46 +0200

    Add launcher between applications and data transfer

---
+++ ProtocolDemo/Launcher/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Launcher
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Launch_Click(object sender, RoutedEventArgs e)
+        {
+            var uriString = string.Format("demo-launch:MainPage/{0}", tbDataToSend.Text);
+            var uri = new Uri(uriString);
+            await Windows.System.Launcher.LaunchUriAsync(uri);
+        }
+    }
+}
